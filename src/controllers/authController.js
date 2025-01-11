4fa11aa28ca08f141432d6517716dc0ee9317501
Khursandzoda Adham <khursandzodaadham20012@gmail.com>
2025-01-12 02:10:02 +0300

    V.2.0.3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,8 +12,13 @@ export const registerUser = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     // Если есть ошибки, возвращаем их клиенту
-    return res.status(400).json({ errors: errors.array() });
+    const err = errors.array();
+    //console.log(err[0]?.msg)
+    return res.status(400).json({ errors: err[0]?.msg });
     }
+
+   
+
 
   const { username, email, password, captchaToken } = req.body;
     //console.log(captchaToken);
@@ -83,7 +88,11 @@ export const loginUser = async (req, res) => {
 
   try {
     const [rows] = await db.query('SELECT * FROM User WHERE email = ?', [email]);
-    if (rows.length === 0) return res.status(404).json({ message: 'Нет доступа' });
+
+
+    
+
+    if (rows.length === 0) return res.status(404).json({ message: `${ email } Не зарегистрирован!` });
 
     // Если пользователь найден, извлекаем его данные (например, хэшированный пароль)
     const user = rows[0];
@@ -117,18 +126,20 @@ export const loginUser = async (req, res) => {
 
 export const homeUser = async (req, res) => {
 
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  //id с заналовка при получение его после authorization 
   
-  const { id } = req.params;
-  //console.log(id)
-
-  if (!token) return res.status(401).json({ message: 'Нет токена авторизации' });
   try {
 
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    //id с заналовка при получение его после authorization 
+    
+    const { id } = req.params;
+    //console.log(id)
+   
+  if (!token) return res.status(401).json({ message: 'Нет токена авторизации' });
+
     const [rows] = await db.query('SELECT * FROM User WHERE id = ?', [id]);
-    if (rows.length === 0) return res.status(404).json({ message: 'Нет доступа' });
+    if (rows.length === 0) return res.status(404).json({ message: 'Прошизашла ошибка попоробуйте перезайти!' });
 
     //console.log(rows)
     res.json({rows});
